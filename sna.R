2e1d3a3617e3f1cc1c8dd9e49cd594d222e34f9f
Charles Hans Thompson <hans.thompson1@gmail.com>
2014-08-29 15:24:46 -0800

    first plot

--- sna.R
+++ sna.R
@@ -1,11 +1,18 @@
 library(plyr)
 library(dplyr)
 library(lubridate)
+library(igraph)
+
 
 load("gov_data.rda")
 
+
+
+
 income  <- filter(gov_data, Amount > 0 )
 expense <- filter(gov_data, Amount < 0 )
+
+connections <- 15
 
 income$full_name <- factor(income$full_name)
 
@@ -13,6 +20,6 @@ ddply(income, .(full_name, Group.Candidate.Name), function(x) {sum(x$Amount)} )
 
 tidy_income <- ddply(income, .(full_name, Group.Candidate.Name), function(x) {sum(x$Amount)} )
 
-tidy_income[table(tidy_income$full_name) > 15,]
+giver_nodes <- names(table(tidy_income$full_name))[table(tidy_income$full_name) > connections]
 
-
+tidy_income[tidy_income$full_name %in% giver_nodes,]
