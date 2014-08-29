93fd8cd05861c689653b3faf0b04bcab16ef075c
Charles Hans Thompson <hans.thompson1@gmail.com>
2014-08-29 15:25:20 -0800

    first plot (for reals)

--- sna.R
+++ sna.R
@@ -12,14 +12,16 @@ load("gov_data.rda")
 income  <- filter(gov_data, Amount > 0 )
 expense <- filter(gov_data, Amount < 0 )
 
-connections <- 15
+connections <- 25
 
 income$full_name <- factor(income$full_name)
-
-ddply(income, .(full_name, Group.Candidate.Name), function(x) {sum(x$Amount)} )
 
 tidy_income <- ddply(income, .(full_name, Group.Candidate.Name), function(x) {sum(x$Amount)} )
 
 giver_nodes <- names(table(tidy_income$full_name))[table(tidy_income$full_name) > connections]
 
-tidy_income[tidy_income$full_name %in% giver_nodes,]
+some_income <- tidy_income[tidy_income$full_name %in% giver_nodes,]
+
+network <- graph.data.frame(some_income)
+
+plot(network)
